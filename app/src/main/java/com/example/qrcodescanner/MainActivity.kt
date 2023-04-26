3097ad9160610fff30d3ca03fa2cf6d6facf5741
Nooruz21 <nooruz21n@gmail.com>
2023-04-26 12:18:04 +0600

    add barcodeScan

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.qrcodescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qrcodescanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var requestCamera: ActivityResultLauncher<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestCamera = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                val intent = Intent(this, BarcodeScan::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.btnBc.setOnClickListener {
+            requestCamera?.launch(android.Manifest.permission.CAMERA)
+        }
     }
 }
